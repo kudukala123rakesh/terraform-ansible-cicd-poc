742e0ab276e6c3b8c9f943547896005720be96f6
EC2 Default User <ec2-user@ip-172-31-26-173.eu-north-1.compute.internal>
2026-01-12 14:44:22 +0000

    new updates

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,18 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Generate SSH key
-resource "tls_private_key" "ec2_key" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-# Create AWS key pair
-resource "aws_key_pair" "terraform_key" {
-  key_name   = "terraform-key"
-  public_key = tls_private_key.ec2_key.public_key_openssh
-}
-
 # Create EC2 instance
 resource "aws_instance" "demo_ec2" {
   ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 (us-east-1)
